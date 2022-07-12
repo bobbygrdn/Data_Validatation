1b2227a41093e8f5c16ecd9ec721daef3798d331
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-12 07:54:14 -0700

    Added main algorithm comments

---
+++ app.js
@@ -0,0 +1,5 @@
+// These words cannot be apart of the comments given:
+// zoom, channel, galvanize, mcsp, garrett, ross, incompetent, unfunny
+
+// If any of these words appear in a comment, they need to be blocked out with dashes eg. ('z---')
+
